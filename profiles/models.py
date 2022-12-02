7030fa04309862b185a4c4199a0e801bf4ea52fd
seanreid1997 <reid.sean14@yahoo.com>
2022-12-02 23:02:42 +0000

    Create profile model to test database

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    temp docstring
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="profile")
+    username = models.CharField(max_length=80)
+    id_user = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    profile_image = CloudinaryField('image', default='https://t3.ftcdn.net/jpg/03/46/83/96/360_\
+    F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg')
+
+    def __str__(self):
+        return self.user.username
+
